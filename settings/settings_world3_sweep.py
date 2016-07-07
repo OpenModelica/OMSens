f0cd4b0c611437e7badcc52bba8c1e996a97e6d5
Alejandro Danós <adp007@gmail.com>
2016-07-06 22:26:01 -0300

    added different paths for different modelica versions

--- settings/settings_world3_sweep.py
+++ settings/settings_world3_sweep.py
@@ -7,9 +7,14 @@ _world3_scenario_model_skeleton = "SystemDynamics.WorldDynamics.World3.Scenario_
 _currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 _parentdir = files_aux.parentDir(_currentdir)
 _resource_path =os.path.join(_parentdir,"resource")
-_sys_dyn_package_path = os.path.join(os.path.join(_resource_path,"SystemDynamics"),"package.mo")
 _std_run_csv = os.path.join(_resource_path,"standard_run.csv")
 
 # CAREFUL: both must be equivalent!! (because of differences between modelica and python we can't merge them into one)
 csv_file_name_python_skeleton = "iter_{i_str}.csv"
 csv_file_name_modelica_skeleton= """ "iter_" + String(i) + ".csv";"""
+
+#System Dynamics versions:
+_sys_dyn_package_path = os.path.join(os.path.join(_resource_path,"SystemDynamics"),"package.mo")
+_sys_dyn_package_pw_fix_path = os.path.join(os.path.join(os.path.join(os.path.join(_resource_path,"sys_dyn"),"pw_fix"),"SystemDynamics"),"package.mo")
+_sys_dyn_package_vanilla_path = os.path.join(os.path.join(os.path.join(os.path.join(_resource_path,"sys_dyn"),"vanilla"),"SystemDynamics"),"package.mo")
+_sys_dyn_package_pop_state_var_new = os.path.join(os.path.join(os.path.join(os.path.join(_resource_path,"sys_dyn"),"pop_state_var_new"),"SystemDynamics"),"package.mo")
