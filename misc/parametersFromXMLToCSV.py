8b09c7084f199c27ac31632c216842d3edecd043
Alejandro Danós <adp007@gmail.com>
2016-10-04 12:29:44 -0300

    added try:except: for when the xml is missing

--- misc/parametersFromXMLToCSV.py
+++ misc/parametersFromXMLToCSV.py
@@ -3,7 +3,12 @@
 # 2) Gets the root parameters
 # 3) For all of the root parameters, it prints its name and its value as csv
 from xml.dom import minidom
-xmldoc = minidom.parse('resource/standard_run.xml')
+import sys
+try:
+    xmldoc = minidom.parse('resource/standard_run.xml')
+except FileNotFoundError:
+    print("ERROR: XML NOT FOUND! Run from root of modelicascripts project and not inside this folder so the path to read the .xml makes sense to the script")
+    sys.exit(1)
 itemlist = xmldoc.getElementsByTagName('ScalarVariable')
 root_parameters_nodes = [x for x in itemlist if "." not in x.getAttribute("name") and x.getAttribute("variability")=="parameter"] #doesn't contain a dot and is of variability parameter
 print("Parameter name, value")
