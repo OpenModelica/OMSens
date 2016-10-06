d1347d858df6717be47a157b73309593d3f6d6e5
Alejandro Danós <adp007@gmail.com>
2016-10-06 16:17:39 -0300

    added ticks between min and max by range of 10^i

--- HEAT_MAP_INICIAL.py
+++ HEAT_MAP_INICIAL.py
@@ -2,30 +2,79 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import SymLogNorm   # for logarithmic scale
 import pandas as pd
 import numpy as np
+import math
 
 # GLOBALS
 input_matrix_path = "resource/w3_only1992_time_fix_paramvarmatrix.csv"
+### Choose one of the following. The first is for Logarithmic scale and the second for linear scale
+heatmap_norm_function = lambda vmin, vmax, linthresh: SymLogNorm(vmin=vmin, vmax=vmax,linthresh=linthresh)
+### Uncomment the following if you want to run with LOGARITHMIC SCALE
+heatmap_norm_function = lambda vmin, vmax, linthresh: SymLogNorm(vmin=vmin, vmax=vmax,linthresh=linthresh)
+
 linthresh = 1.0 #Since the logarithm of values close to zero tends toward infinity, a small range around zero needs to be mapped linearly. The parameter linthresh allows the user to specify the size of this range (-linthresh, linthresh). The size of this range in the colormap is set by linscale. When linscale == 1.0 (the default), the space used for the positive and negative halves of the linear range will be equal to one decade in the logarithmic range.
 
-# page = urlopen("http://datasets.flowingdata.com/ppg2008.csv")
-# data = pd.read_csv("asd.csv", index_col=0)
+# Aux:
+# Function to get numbers in an exponential range (used in the ticks in the colorbar of the colormap)
+def exponentialRangeFromMinAndMax(min_num,max_num):
+    res_range = []
+    if min_num == 0:
+        largest_positive_power_of_ten_smaller_than_min = 0
+    else:
+        min_SymLog = math.floor(math.log10(min_num)) if min_num > 0 else math.floor(math.log10(abs(min_num))) # symlog because it can take negative numbers
+        if min_SymLog <0:
+            # The number is between -1 and 0 or between 0 and 1
+            if min_num <0:
+                # -1 < number < 0
+                largest_positive_power_of_ten_smaller_than_min = -1
+            else:
+                # 0 < number < 1
+                largest_positive_power_of_ten_smaller_than_min = 0
+        else:
+            # number <= -1 or 1 <= number
+            largest_positive_power_of_ten_smaller_than_min = math.copysign(10**min_SymLog,min_num)
+    if max_num == 0:
+        smallest_power_of_ten_larger_than_max = 0
+    else:
+        max_SymLog = math.ceil(math.log10(max_num)) if max_num > 0 else math.ceil(math.log10(abs(max_num))) # symlog because it can take negative numbers
+        if max_SymLog <0:
+            # The number is between -1 and 0 or between 0 and 1
+            if max_num <0:
+                # -1 < number < 0
+                smallest_power_of_ten_larger_than_max = 0
+            else:
+                # 0 < number < 1
+                smallest_power_of_ten_larger_than_max= 1
+        else:
+            # number <= -1 or 1 <= number
+            smallest_power_of_ten_larger_than_max = math.copysign(10**max_SymLog,max_num)
+
+    print("min_num"+str(min_num))
+    print("max_num"+str(max_num))
+    print("largest_positive_power_of_ten_smaller_than_min"+str(largest_positive_power_of_ten_smaller_than_min))
+    print("smallest_power_of_ten_larger_than_max"+str(smallest_power_of_ten_larger_than_max))
+    accum = largest_positive_power_of_ten_smaller_than_min
+    while accum <= smallest_power_of_ten_larger_than_max:
+        while accum <= -1:
+            res_range.append(accum)
+            accum = accum/10
+        if accum <= smallest_power_of_ten_larger_than_max:
+            res_range.append(0)
+            accum = 1
+            while accum <= smallest_power_of_ten_larger_than_max:
+                res_range.append(accum)
+                accum = accum*10
+    return res_range
+# Start of code
 data = pd.read_csv(input_matrix_path, index_col=0)
-# nba = pd.read_csv(page, index_col=0)
-
-# Normalize data columns
-# nba_norm = (nba - nba.mean()) / (nba.max() - nba.min())
-
-# Sort data according to Points, lowest to highest
-# This was just a design choice made by Yau
-# inplace=False (default) ->thanks SO user d1337
 
 # Plot it out
 fig, ax = plt.subplots()
 min_of_all = data.min().min()   # the first min returns a series of all the mins. The second min returns the min of the mins
 max_of_all = data.max().max()   # the first max returns a series of all the maxs. The second max returns the max of the max
 heatmap = plt.pcolor(data, norm=SymLogNorm(vmin=min_of_all, vmax=max_of_all,linthresh=linthresh))
+colorbar_ticks = exponentialRangeFromMinAndMax(min_of_all,max_of_all)
 
-plt.colorbar()
+cbar = plt.colorbar(ticks=colorbar_ticks)
 
 # Format
 fig = plt.gcf()
@@ -42,14 +91,6 @@ ax.set_xticks(np.arange(data.shape[1]) + 0.5, minor=False)
 ax.invert_yaxis()
 ax.xaxis.tick_top()
 
-# Set the labels
-
-# label source:https://en.wikipedia.org/wiki/Basketball_statistics
-# labels = [
-#     'Games', 'Minutes', 'Points', 'Field goals made', 'Field goal attempts', 'Field goal percentage', 'Free throws made', 'Free throws attempts', 'Free throws percentage',
-    # 'Three-pointers made', 'Three-point attempt', 'Three-point percentage', 'Offensive rebounds', 'Defensive rebounds', 'Total rebounds', 'Assists', 'Steals', 'Blocks', 'Turnover', 'Personal foul']
-
-# note I could have used nba_sort.columns but made "labels" instead
 ax.set_xticklabels(data.columns, minor=False)
 ax.set_yticklabels(data.index, minor=False)
 
