26393eb9b1286afcebd515432938fdac171d0bce
Alejandro Danós <adp007@gmail.com>
2016-10-11 12:33:52 -0300

    changed axis ticks labels to ids instead of the full name

--- HEAT_MAP_INICIAL.py
+++ HEAT_MAP_INICIAL.py
@@ -4,16 +4,30 @@ import matplotlib.ticker                   # to set a special formatter for the 
 import pandas as pd
 import numpy as np
 import math
+import os
+
+# Mine
+import world3_specific.standard_run_params_defaults
+
+##### THIS MODULE WAS MADE ON A RUSH AND NEEDS BEAUTIFICATION      ################
 
 # GLOBALS
-# input_matrix_path = "resource/w3_only1992_time_fix_paramvarmatrix.csv"
-input_matrix_path = "resource/w3_only1901_time_fix_paramvarmatrix.csv"
-# plot_path = "tmp/asd_sin_sort.png"
-plot_path = "tmp/asd_con_sort.png"
+input_matrix_path = "resource/w3_only1992_time_fix_paramvarmatrix.csv"
+# input_matrix_path = "resource/w3_only1901_time_fix_paramvarmatrix.csv"
+plot_folder_path = "tmp"
+# plot_name = "asd_sin_sort.png"
+plot_name = "asd_con_sort.png"
 
 linthresh = 1.0 #Since the logarithm of values close to zero tends toward infinity, a small range around zero needs to be mapped linearly. The parameter linthresh allows the user to specify the size of this range (-linthresh, linthresh). The size of this range in the colormap is set by linscale. When linscale == 1.0 (the default), the space used for the positive and negative halves of the linear range will be equal to one decade in the logarithmic range.
 
 # Aux:
+# Function to abbreviate variables and parameters to their id ("population" -> "V.103")
+def abbreviateStringsUsingDict(strs_list,abbr_dict):
+    abbreviated_indices = []
+    for long_str in strs_list:
+        abbreviated_indices.append(abbr_dict[long_str])
+    return abbreviated_indices
+
 # Function to get numbers in an exponential range (used in the ticks in the colorbar of the colormap)
 def exponentialRangeFromMinAndMax(min_num,max_num):
     res_range = []
@@ -67,6 +81,10 @@ data.sort_index(inplace=True)
 # Sort data's columns
 data.sort_index(axis=1,inplace=True)
 
+### Abbreviate parameters and vars to their IDs from world3_specific/(?).py so the info fits better in the heatmap
+abbreviated_columns = abbreviateStringsUsingDict(data.columns,world3_specific.standard_run_params_defaults.om_TheoParamSensitivity_differentiableVariables_dict)
+abbreviated_indices = abbreviateStringsUsingDict(data.index,world3_specific.standard_run_params_defaults.om_TheoParamSensitivity_params_dict)
+
 # Plot it out
 fig, ax = plt.subplots()
 min_of_all = data.min().min()   # the first min returns a series of all the mins. The second min returns the min of the mins
@@ -75,6 +93,7 @@ heatmap = plt.pcolor(data, norm=SymLogNorm(vmin=min_of_all, vmax=max_of_all,lint
 colorbar_ticks = exponentialRangeFromMinAndMax(min_of_all,max_of_all)
 
 cbar = plt.colorbar(ticks=colorbar_ticks,format=matplotlib.ticker.FuncFormatter(lambda x, p: "%.0e" % x))
+cbar.ax.tick_params(labelsize=10)
 
 # Format
 fig = plt.gcf()
@@ -91,11 +110,18 @@ ax.set_xticks(np.arange(data.shape[1]) + 0.5, minor=False)
 ax.invert_yaxis()
 ax.xaxis.tick_top()
 
-ax.set_xticklabels(data.columns, minor=False)
-ax.set_yticklabels(data.index, minor=False)
+## With long string names
+# ax.set_xticklabels(data.columns, minor=False)
+# ax.set_yticklabels(data.index, minor=False)
+## With abbreviated string names
+ax.set_xticklabels(abbreviated_columns, minor=False, fontsize=8)
+ax.set_yticklabels(abbreviated_indices, minor=False, fontsize=6)
 
-# rotate the
+# Rotate the ticks labels in the x and y axis
+# plt.xticks(rotation=80)
 plt.xticks(rotation=90)
+# plt.yticks(rotation=-15)
+plt.yticks(rotation=0)
 
 ax.grid(False)
 
@@ -109,5 +135,9 @@ for t in ax.yaxis.get_major_ticks():
     t.tick1On = False
     t.tick2On = False
 
+# Tight layout to maximize usage of space
+plt.tight_layout()
+
 # plt.show()
+plot_path = os.path.join(plot_folder_path,plot_name)
 plt.savefig(plot_path)
