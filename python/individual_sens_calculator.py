f5f4577d0cc78a26ff1ef322517507bd3e857074
Ale Danós <alejandro.danos@gmail.com>
2018-04-17 18:39:50 -0300

    first start to finish final version of indiv sens analyzis.py without w3 hardcoded

--- python/individual_sens_calculator.py
+++ python/individual_sens_calculator.py
@@ -26,11 +26,18 @@ def main():
     # Make tmp dir
     output_folder_path = files_aux.makeOutputPath()
     # Set relative paths as absolute paths using this scripts path as base
+    #  Folders
     current_dir   = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     resource_path = os.path.join(current_dir,"resource")
-    mo_file       = os.path.join(resource_path,full_json["model_mo_path"])
-    output_mos_name = "runPerturbingIndividually.mos"
-    output_mos_path = os.path.join(output_folder_path,output_mos_name)
+    #  Files
+    #   Modelica model file
+    mo_file          = os.path.join(resource_path,full_json["model_mo_path"])
+    #   .mos script
+    output_mos_name  = "runPerturbingIndividually.mos"
+    output_mos_path  = os.path.join(output_folder_path,output_mos_name)
+    #   Standard run csv
+    std_run_filename = "std_run.csv"
+    std_run_path     = os.path.join(output_folder_path,std_run_filename)
     # Generate list of params and their perturbed values from their defaults and a percentage to perturb
     parameters_to_perturbate_tuples = [(p_name,p_val,p_val*full_json["percentage"]/100) for p_name,p_val in zip(full_json["param_names"], full_json["param_vals"])]
     # Set .mos creator arguments
@@ -42,6 +49,7 @@ def main():
         "parameters_to_perturbate_tuples" : parameters_to_perturbate_tuples,
         "output_mos_path"                 : output_mos_path,
         "csv_file_name_modelica_function" : world3_settings.calc_sens_csv_file_name_function,
+        "std_run_filename"                : std_run_filename,
     }
     # Call .mos creator
     mos_writer.calculate_sensitivities_mos_writer.createMos(**mos_creator_kwargs)
@@ -52,21 +60,23 @@ def main():
     logger.info("Running Modelica with specified information")
 # DESCOMENTAR:
     running.run_omc.runMosScript(output_mos_path)
-    # # Get csvs paths and info pairs
-    # perturbed_csvs_path_and_info_pairs = w3_sens_calculator.csvPathAndParameterNameForFolderAndParametersInfo(output_folder_path,parameters_to_perturbate_tuples)
-    # # Calculate sensitivities
-    # analyze_csvs_kwargs = {
-    #     "perturbed_csvs_path_and_info_pairs" : perturbed_csvs_path_and_info_pairs,
-    #     "std_run_csv_path"                   : "resource/standard_run.csv",
-    #     "target_vars_list"                   : target_vars_list,
-    #     "percentage_perturbed"               : percentage,
-    #     "specific_year"                      : year_target,
-    #     "output_folder_analyses_path"        : output_folder_path,
-    #     "rms_first_year"                     : full_json["start_time"],
-    #     "rms_last_year"                      : full_json["stop_time"],
-    # }
-    # logger.info("Analyzing variable sensitivities to parameters from CSVs")
-    # analysis.sensitivities_to_parameters_analysis_from_csv.analyzeSensitivitiesFromManyVariablesToParametersAndCreateParamVarMatrices(**analyze_csvs_kwargs)
+    # Get csvs paths and info pairs
+    perturbed_csvs_path_and_info_pairs = w3_sens_calculator.csvPathAndParameterNameForFolderAndParametersInfo(output_folder_path,parameters_to_perturbate_tuples)
+    # Assume that the .csv with the standard run data will be called like the following
+    # Get the path of the recently ran standard run
+    # Calculate sensitivities
+    analyze_csvs_kwargs = {
+        "perturbed_csvs_path_and_info_pairs" : perturbed_csvs_path_and_info_pairs,
+        "std_run_csv_path"                   : std_run_path,
+        "target_vars_list"                   : full_json["vars_to_analyze"],
+        "percentage_perturbed"               : full_json["percentage"],
+        "specific_year"                      : full_json["stop_time"],
+        "output_folder_analyses_path"        : output_folder_path,
+        "rms_first_year"                     : full_json["start_time"],
+        "rms_last_year"                      : full_json["stop_time"],
+    }
+    logger.info("Analyzing variable sensitivities to parameters from CSVs")
+    analysis.sensitivities_to_parameters_analysis_from_csv.analyzeSensitivitiesFromManyVariablesToParametersAndCreateParamVarMatrices(**analyze_csvs_kwargs)
 # DESCOMENTAR^
 
 # FIRST EXECUTABLE CODE:
