2255c514f1791615d7b80286350650e5bab250ac
adanos <ale@mail.com>
2018-05-18 10:12:04 -0300

    minor changes w3 sens calc

--- python/individual_sens_calculator.py
+++ python/individual_sens_calculator.py
@@ -7,11 +7,11 @@ import sys
 # Ours
 import analysis.indiv_sens
 import filesystem.files_aux as files_aux
-import mos_writer.calculate_sensitivities_mos_writer
+import mos_writer.calculate_sensitivities_mos_writer as sens_mos_writer
 import running.run_omc
 # Setup logging
 import settings.gral_settings
-from settings import settings_world3_sweep as world3_settings
+import argparse
 
 logger = logging.getLogger("-Individual Sens Calculator-")
 
@@ -19,54 +19,25 @@ logger = logging.getLogger("-Individual Sens Calculator-")
 def main():
     # Logging settings
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    # json_file_path = getCommandLineArguments()
     # Args
-    json_path = "resource/test_files/individual/test_06.json"
-    # Read json
-    logger.info("Reading individual sensitivities test file in path {0}.".format(json_path))
-    with open(json_path, 'r') as fp:
-        full_json = json.load(fp)
-    # Mo file from json
-    json_mo_path = full_json["model_mo_path"]
-    # Check if it's absolute path or relative path and act accordingly
-    is_abs_path = os.path.isabs(json_mo_path)
-    if is_abs_path:
-        # If it's already an absolute path, there's nothing to do
-        mo_file_path = json_mo_path
-    else:
-        # If it's a relative path, make it absolute
-        mo_file_path = os.path.abspath(json_mo_path)
-    # Default file names
-    output_mos_name = "runPerturbingIndividually.mos"
+    json_file_path = "resource/test_files/individual/test_00.json"
+    output_mos_name = "indiv_sens_sims.mos"
     std_run_filename = "std_run.csv"
-    # Get files paths in output folder
     output_folder_path, output_mos_path, std_run_path = filesPathsInOutputFolder(output_mos_name, std_run_filename)
-    # Generate list of params and their perturbed values from their defaults and a percentage to perturb
-    parameters_to_perturbate_tuples = listOfParametersPerturbationInfo(full_json["param_names"],
-                                                                       full_json["param_vals"], full_json["percentage"])
-    # Set .mos creator arguments
-    mos_creator_kwargs = {
-        "model_name": full_json["model_name"],
-        "mo_file": mo_file_path,
-        "startTime": full_json["start_time"],
-        "stopTime": full_json["stop_time"],
-        "parameters_to_perturbate_tuples": parameters_to_perturbate_tuples,
-        "output_mos_path": output_mos_path,
-        "csv_file_name_modelica_function": settings.gral_settings.calc_sens_csv_file_name_function,
-        "std_run_filename": std_run_filename,
-    }
-    # Call .mos creator
-    mos_writer.calculate_sensitivities_mos_writer.createMos(**mos_creator_kwargs)
+    sens_mos_writer.createMosFromJSON(json_file_path, output_mos_path, std_run_filename)
     # Run .mos
     # ADAPTAR INFO PARA QUE DIGA TODA LA INFO A CORRER (modelo, path, etc):
     # logger.info("Calculating empirical parameter sensitivities for percentage {perc}, for all of the differentiable variables in W3 and target year {year_target}".format(perc=full_json["percentage"])
     # ADAPTAR INFO PARA QUE DIGA TODA LA INFO A CORRER (modelo, path, etc)^
     logger.info("Running Modelica with specified information")
     running.run_omc.runMosScript(output_mos_path)
+    # Read json
+    with open(json_file_path, 'r') as fp:
+        full_json = json.load(fp)
     # Get csvs paths and info pairs
     perturbed_csvs_path_and_info_pairs = csvPathAndParameterNameForFolderAndParametersInfo(
         output_folder_path, parameters_to_perturbate_tuples)
-    # Assume that the .csv with the standard run data will be called like the following
-    # Get the path of the recently ran standard run
     # Calculate sensitivities
     analyze_csvs_kwargs = {
         "perturbed_csvs_path_and_info_pairs": perturbed_csvs_path_and_info_pairs,
@@ -112,6 +83,13 @@ def csvPathAndParameterNameForFolderAndParametersInfo(output_folder_path, parame
         csv_path = os.path.join(output_folder_path, csv_name)
         perturbed_csvs_path_and_info_pairs.append((csv_path, param_info))
     return perturbed_csvs_path_and_info_pairs
+
+def getCommandLineArguments():
+    parser = argparse.ArgumentParser(description=script_description)
+    parser.add_argument('test_file_path', metavar='test_file_path',
+                        help='The file path to the test file containing the CSVs to plot, the variables, the title, etc.')
+    args = parser.parse_args()
+    return args.test_file_path
 
 
 # FIRST EXECUTABLE CODE:
