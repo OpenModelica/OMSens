b3e2a482ab4e481e4777cf40d5d78a336e204e93
Alechan <user@mail.com>
2018-08-19 14:49:13 -0300

    build model: fixed test

--- python/testing/test_build_model.py
+++ python/testing/test_build_model.py
@@ -31,28 +31,16 @@ class TestsBuildModel(unittest.TestCase):
         model_name = "Model"
         start_time = 1
         stop_time  = 2
-        # 1) Create mos that builds model
-        # 1.1) Load Modelica library
-        load_modelica_str = "loadModel(Modelica);"
-        # 1.2) Load file for model
-        load_model_file_skeleton = "loadFile({0}); getErrorString();"
-        load_model_file_str = load_model_file_skeleton.format(model_name)
-        # 1.3) Build model
-        build_model_skeleton = """buildModel({0}, startTime={1},stopTime={2},outputFormat="csv"); getErrorString();"""
-        build_model_str = build_model_skeleton.format(model_name,start_time,stop_time)
-        # MOS Script string
-        mos_script_parts = [load_modelica_str, load_model_file_str, build_model_str]
-        mos_script_string = "\n".join(mos_script_parts)
-        # ADAPTAR LO DE ARRIBA
+        # Initialize and call model builder
         test_model_builder = build_model.ModelicaModelBuilder(model_name, start_time, stop_time, model_file_path)
         test_model_builder.buildToFolderPath(self._temp_dir)
         # Get script extensions regex
-        regex = '.*\.mos$'
+        regex = "{0}".format(model_name)
         # Get list of files from regex
         files_in_dir = os.listdir(self._temp_dir)
-        plot_files = [x for x in files_in_dir if re.match(regex, x)]
+        files_for_regex = [x for x in files_in_dir if re.match(regex, x)]
         # Check that there is at least one file for regex
-        if len(plot_files) < 1:
+        if len(files_for_regex) < 1:
             error_msg = "The model builder should create at least one file in folder."
             self.fail(error_msg)
 
