7b5ad52e75bae95eaf60261a3323574f6e74e339
Ale Danós <alejandro.danos@gmail.com>
2018-08-24 15:58:35 -0300

    sweep: more advancements but still no round version

--- python/running/sweep.py
+++ python/running/sweep.py
@@ -10,33 +10,54 @@ class ParametersSweeper():
         self.start_time = start_time
         self.stop_time = stop_time
         self.perturbation_info_per_param = perturbation_info_per_param
-        # Calculate the values per param
-        self.values_per_param = valuesPerParamFromParamsInfos(self.perturbation_info_per_param)
         # Initialize builder
         self.model_builder = build_model.ModelicaModelBuilder(model_name, start_time, stop_time, model_file_path)
 
     def runSweep(self, dest_folder_path):
+        # Build model
         compiled_model = self.model_builder.buildToFolderPath(dest_folder_path)
+        # Get the default values for the params to perturb using the compiled model
+        params_defaults = self.defaultValuesForParamsToPerturb(compiled_model)
+
+        # Calculate the values per param
+        values_per_param = valuesPerParamFromParamsInfos(params_defaults,self.perturbation_info_per_param)
         # Iterate combinations of parameters values
 
         # Create Sweep Results
         pass
 
+    # Instance auxs
+    def defaultValuesForParamsToPerturb(self,compiled_model):
+        # Get list of params to perturb
+        params_to_perturb = [ x["name"] for x in self.perturbation_info_per_param ]
+        # Using the compiled model, ask for the default value of each one
+        params_defaults = {}
+        for p in params_to_perturb:
+            p_def_val = compiled_model.defaultParameterValue(p)
+            params_defaults[p] = p_def_val
+        return params_defaults
+
+
+
 
 # Aux
-def valuesPerParamFromParamsInfos(perturbation_info_per_param):
+def valuesPerParamFromParamsInfos(params_defaults, perturbation_info_per_param):
     values_per_param = []
-    for p_info in perturbation_info_per_param
-        param_values = valuesForParameterFromParamInfo(p_info)
-        values_per_param.append(param_values)
+    for p_info in perturbation_info_per_param:
+        perturbed_param_values = perturbedValuesForParams(p_info,params_defaults)
+        values_per_param.append(perturbed_param_values)
     return values_per_param
 
 
-def valuesForParameterFromParamInfo(param_info):
-    initial_value = param_info["initial_val"]
-    delta = param_info["delta_percentage"]
+def perturbedValuesForParams(param_info, params_defaults):
+    # Disaggregate param info
+    param_name = param_info["name"]
+    delta      = param_info["delta_percentage"]
     iterations = param_info["iterations"]
-    lambda_i = lambda i: initial_value * (1 - (iterations / 2) * delta) + initial_value * delta * i
+    def_value  = params_defaults[param_name]
+    # Calculate values for this param
+
+    lambda_i = lambda i: def_value * (1 - (iterations / 2) * delta) + def_value * delta * i
     values = [lambda_i(i) for i in range(iterations)]
     param_values = \
         {
