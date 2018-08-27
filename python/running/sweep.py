ad0ab8f551ba48bc3da6edf4d3e5a5ea93283fec
adanos <ale@mail.com>
2018-08-27 20:23:15 -0300

    sweep: first version of runSweep

--- python/running/sweep.py
+++ python/running/sweep.py
@@ -1,8 +1,11 @@
 # Std
 import numpy
 import itertools
+import os
 # Mine
 import modelica_interface.build_model as build_model
+import filesystem.files_aux as files_aux
+import running.simulation_run_info as simu_run_info
 
 
 class ParametersSweeper():
@@ -26,8 +29,45 @@ class ParametersSweeper():
         self.params_vals_combinations = dict_product(self.values_per_param)
 
     def runSweep(self, dest_folder_path):
-        # Create Sweep Results
-        pass
+        # Make folder for runs
+        runs_folder_name = "runs"
+        runs_folder_path = os.path.join(dest_folder_path, runs_folder_name)
+        files_aux.makeFolderWithPath(runs_folder_path)
+        # Run STD run
+        std_run_name = "std_run.csv"
+        std_run_path = os.path.join(runs_folder_path, std_run_name)
+        std_run_results = self.compiled_model.simulate(std_run_path)
+        # Make dir for perturbed runs
+        perturbed_runs_folder_name = "runs"
+        perturbed_runs_folder_path = os.path.join(dest_folder_path, perturbed_runs_folder_name)
+        files_aux.makeFolderWithPath(perturbed_runs_folder_path)
+        # Run the different values combinations
+        sweep_iterations = []
+        for i in range(len(list(self.params_vals_combinations))):
+            vals_comb = self.params_vals_combinations[i]
+            # Perturb the parameters for this iteration
+            swept_params_info = []
+            for param_name in vals_comb:
+                # Instantiate perturbed params infos
+                param_default_val = self.params_defaults[param_name]
+                param_perturbed_val = vals_comb[param_name]
+                perturbed_param_info = simu_run_info.PerturbedParameterInfo(param_name, param_default_val,
+                                                                            param_perturbed_val)
+                # Save the perturbed param info
+                swept_params_info.append(perturbed_param_info)
+                # Change the value in the model
+                self.compiled_model.setParameterStartValue(param_name, vals_comb[param_name])
+            # Run the simulation
+            simu_csv_name = "run_{0}.csv".format(i)
+            simu_csv_path = os.path.join(perturbed_runs_folder_path, simu_csv_name)
+            simu_results = self.compiled_model.simulate(simu_csv_path)
+            # Instantiate sweep iteration results
+            sweep_iter_results = SweepIterationResults(simu_results, swept_params_info)
+            # Add results to list
+            sweep_iterations.append(sweep_iter_results)
+        # Instantiate sweep results
+        swept_params_names = [x["name"] for x in self.perturbation_info_per_param]
+        ParametersSweepResults(self.model_name, [], std_run_results, sweep_iterations)
 
     def valuesPerParameter(self):
         return self.values_per_param
