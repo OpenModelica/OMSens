801d3627daf1135f6ddd097fa6cc47519a6d1505
Alechan <user@mail.com>
2018-08-31 14:02:58 -0300

    sweep: added test to run all predefined JSONs

--- python/testing/test_predefined_json_files.py
+++ python/testing/test_predefined_json_files.py
@@ -7,9 +7,9 @@ import unittest
 
 import pytest
 
-import analysis.indiv_sens
 import filesystem.files_aux
 import mos_writer.calculate_sensitivities_mos_writer as sens_mos_writer
+import multiparam_sweep
 
 
 class TestsRunOMC(unittest.TestCase):
@@ -44,3 +44,23 @@ class TestsRunOMC(unittest.TestCase):
                     error_msg = str(e)
                     self.fail("The file {0} is an invalid test file. It raised the following exception:\n {1}".format(
                         test_file_path, error_msg))
+
+
+    @pytest.mark.slow
+    def test_run_all_sweep_JSON_files(self):
+        # Get path of where tests are
+        project_root_path = filesystem.files_aux.projectRoot()
+        exp_files_folder_path = os.path.join(project_root_path, "resource", "experiments", "sweep")
+        # General settings for mos creation
+        for exp_file_name in os.listdir(exp_files_folder_path):
+            exp_file_path = os.path.join(exp_files_folder_path, exp_file_name)
+            if os.path.isfile(exp_file_path):
+                try:
+                    multiparam_sweep.sweepAndPlotFromJSON(self._temp_dir,exp_file_path)
+                    # Remove every file so the next test has the folder clean
+                    regex = '.*'
+                    filesystem.files_aux.removeFilesWithRegexAndPath(regex, self._temp_dir)
+                except Exception as e:
+                    error_msg = str(e)
+                    self.fail("The file {0} is an invalid test file. It raised the following exception:\n {1}".format(
+                        exp_file_path, error_msg))
