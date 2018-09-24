4fcf5f6c40da2b5ac34823f917c859edaf926b91
adanos <ale@mail.com>
2018-09-24 18:16:03 -0300

    curvi simplified: added epsilon test

--- python/testing/test_curvi.py
+++ python/testing/test_curvi.py
@@ -29,10 +29,11 @@ class TestIndividualSensitivityAnalysis(unittest.TestCase):
     # Tests:
     def test_curvi_finds_minimum_of_cuadratic(self):
         curvi_mod = self.tryToImportCurviModule()
-        x0, obj_func, epsilon = cuadraticFuncArgs()
-        lower_bounds = [-2]
-        upper_bounds = [2]
+        # Get base args for an example function
+        x0, obj_func, epsilon, lower_bounds, upper_bounds  = baseCuadraticFuncArgs()
+        # Call curvi
         x_opt,f_opt = curvi_mod.curvif_simplified(x0,obj_func,lower_bounds,upper_bounds,epsilon)
+        # Check results
         correct_x_opt = [0]
         if not numpy.isclose(x_opt,correct_x_opt):
             error_msg = "x_opt should be close to {0} but instead it is {1}".format(correct_x_opt,x_opt)
@@ -44,10 +45,13 @@ class TestIndividualSensitivityAnalysis(unittest.TestCase):
 
     def test_curvi_bounds_work(self):
         curvi_mod = self.tryToImportCurviModule()
-        x0, obj_func, epsilon = cuadraticFuncArgs()
+        x0, obj_func, epsilon , lower_bounds, upper_bounds = baseCuadraticFuncArgs()
+        # Redefine the bounds
         lower_bounds = [0.5]
         upper_bounds = [1]
+        # Call curvi
         x_opt,f_opt = curvi_mod.curvif_simplified(x0,obj_func,lower_bounds,upper_bounds,epsilon)
+        # Check results
         correct_x_opt = [0.5]
         if not numpy.isclose(x_opt,correct_x_opt):
             error_msg = "x_opt should be close to {0} but instead it is {1}".format(correct_x_opt,x_opt)
@@ -56,6 +60,32 @@ class TestIndividualSensitivityAnalysis(unittest.TestCase):
         if not numpy.isclose(f_opt,correct_f_opt,0.0001):
             error_msg = "f_opt should be close to {0} but instead it is {1}".format(correct_f_opt,f_opt)
             self.fail(error_msg)
+
+    def test_curvi_epsilon_works(self):
+        curvi_mod = self.tryToImportCurviModule()
+        # Get base args for an example function
+        x0, obj_func, epsilon, lower_bounds, upper_bounds  = baseCuadraticFuncArgs()
+        # Call curvi with a less strict epsilon
+        epsilon_permissive = 0.1
+        x_opt_permissive,f_opt_permissive = curvi_mod.curvif_simplified(x0,obj_func,lower_bounds,upper_bounds,epsilon_permissive)
+        # Call curvi with a MORE strict epsilon
+        epsilon_strict = 0.00001
+        x_opt_strict,f_opt_strict = curvi_mod.curvif_simplified(x0,obj_func,lower_bounds,upper_bounds,epsilon_strict)
+        # Compare X distances to correct answer
+        x_distance_permissive = abs(x_opt_permissive[0])
+        x_distance_strict = abs(x_opt_strict[0])
+        if x_distance_permissive < x_distance_strict:
+            error_msg = "The strict X should be closer to the correct value than the permissive. " \
+                        "X_permissive: {0}. X_strict: {1}".format(x_opt_permissive[0], x_opt_strict[0])
+            self.fail(error_msg)
+        # Compare f(x) distances to correct answer
+        f_distance_permissive = abs(f_opt_permissive)
+        f_distance_strict = abs(f_opt_strict)
+        if f_distance_permissive < f_distance_strict:
+            error_msg = "The strict f(x) should be closer to the correct value than the permissive. " \
+                        "X_permissive: {0}. X_strict: {1}".format(f_opt_permissive, f_opt_strict)
+            self.fail(error_msg)
+
 
     # Auxs:
     def tryToImportCurviModule(self):
@@ -67,8 +97,10 @@ class TestIndividualSensitivityAnalysis(unittest.TestCase):
         return curvif_simplified
 
 
-def cuadraticFuncArgs():
+def baseCuadraticFuncArgs():
     x0 = numpy.array([1])
     obj_func = lambda x: x**2
     epsilon = 0.0001
-    return x0, obj_func, epsilon
+    lower_bounds = [-2]
+    upper_bounds = [2]
+    return x0, obj_func, epsilon, lower_bounds, upper_bounds
