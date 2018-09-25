b6cbcd73b25402e7c52e1e890a84880a1d74b4cd
Alechan <mail@mail.com>
2018-09-25 16:43:19 -0300

    model optimizer: first working version. Needs testing

---
+++ python/testing/test_model_optimizer.py
@@ -0,0 +1,80 @@
+# Std
+import glob
+import os
+import shutil  # para borrar el tempdir
+import tempfile  # para crear el tempdir
+import unittest
+from io import StringIO
+import numpy
+import pathlib
+
+# Mine
+import modelica_interface.build_model as build_model
+import filesystem.files_aux as files_aux
+import vectorial.model_optimizer as model_optimizer_f
+
+
+class TestIndividualSensitivityAnalysis(unittest.TestCase):
+    # setup y teardown de los tests
+    def setUp(self):
+        # Create tempdir and save its path
+        self._temp_dir = tempfile.mkdtemp()
+        self._temp_files = []  # each test case can create individual files
+
+    def tearDown(self):
+        pass
+        shutil.rmtree(self._temp_dir)
+        for f in self._temp_files:
+            f.close()
+    # Tests:
+    def test_one_param_min(self):
+        # Get the base arguments for the 3 params example
+        model_name, start_time, stop_time, model_file_path, target_var_name, parameters_to_perturb, \
+               lower_bounds, upper_bounds, max_or_min, epsilon, build_folder_path = \
+            self.threeParamsModelOptimizerBaseArgsExample()
+        # Replace the args so we only minimize one
+        parameters_to_perturb = ["a"]
+        lower_bounds = [-2]
+        upper_bounds = [2]
+        # Initialize optimizer
+        model_optimizer = model_optimizer_f.ModelOptimizer(model_name, start_time, stop_time, model_file_path,
+                                                           target_var_name, parameters_to_perturb, max_or_min,
+                                                           build_folder_path)
+        # Run optimizer
+        x_opt_dict, f_opt = model_optimizer.optimize(lower_bounds, upper_bounds, epsilon)
+
+
+
+    # Auxs
+    def threeParamsModelOptimizerBaseArgsExample(self):
+        model_name = "Model"
+        model_file_path = os.path.join(self._temp_dir, "model.mo")
+        files_aux.writeStrToFile(model_str, model_file_path)
+        start_time = 0
+        stop_time = 2
+        target_var_name = "x"
+        parameters_to_perturb = ["a", "b", "c"]
+        lower_bounds = [-2, -2, -2]
+        upper_bounds = [ 2,  2,  2]
+        max_or_min = "min"
+        epsilon = 0.001
+        build_folder_path = self._temp_dir
+        return model_name, start_time, stop_time, model_file_path, target_var_name, parameters_to_perturb,\
+               lower_bounds, upper_bounds, max_or_min, epsilon, build_folder_path
+
+
+
+
+
+###########
+# Globals #
+###########
+model_str = \
+ """class Model
+  parameter Real a=1;
+  parameter Real b=1;
+  parameter Real c=1;
+  Real x(fixed=true);
+equation
+  der(x) = a+b+c;
+end Model;"""
---
+++ python/vectorial/model_optimizer.py
@@ -0,0 +1,50 @@
+import modelica_interface.build_model as build_model
+import fortran_interface.curvif_simplified as curvi_mod
+
+class ModelOptimizer():
+    def __init__(self, model_name, start_time, stop_time, model_file_path, target_var_name,
+                 parameters_to_perturb, max_or_min, build_folder_path):
+        # Save args
+        self.model_name            = model_name
+        self.start_time            = start_time
+        self.stop_time             = stop_time
+        self.model_file_path       = model_file_path
+        self.target_var_name       = target_var_name
+        self.parameters_to_perturb = parameters_to_perturb
+        self.max_or_min            = max_or_min
+        # Define members from args
+        # Initialize builder
+        model_builder = build_model.ModelicaModelBuilder(model_name, start_time, stop_time, model_file_path)
+        # Build model
+        compiled_model = model_builder.buildToFolderPath(build_folder_path)
+        self.x0       = [compiled_model.defaultParameterValue(p) for p in parameters_to_perturb]
+        self.obj_func = createObjectiveFunctionForModel(compiled_model, parameters_to_perturb, target_var_name,
+                                                        max_or_min)
+    def optimize(self, lower_bounds, upper_bounds, epsilon):
+        # Run the optimizer
+        x_opt,f_opt = curvi_mod.curvif_simplified(self.x0,self.obj_func,lower_bounds,upper_bounds,
+                                                  epsilon)
+        # Organize the parameters values in a dict
+        x_opt_dict = {self.parameters_to_perturb[i]:x_opt[i] for i in range(len(self.parameters_to_perturb))}
+        return x_opt_dict, f_opt
+
+# Auxs
+def createObjectiveFunctionForModel(compiled_model, param_names, target_var_name, max_or_min):
+    # We initialize a function with "dynamic hardcoded variables". It will have this variables fixed with the value
+    #  from execution context of the function that defined it
+    def objectiveFunction(params_vals):
+        # Set the values for each respective parameter
+        for i in range(len(param_names)):
+            p_name = param_names[i]
+            p_val = params_vals[i]
+            compiled_model.setParameterStartValue(p_name, p_val)
+        # Run the simulation
+        var_val = compiled_model.quickSimulate(target_var_name)
+        # Assign a sign depending if maximizing or minimizing
+        if max_or_min == "max":
+            obj_func_val = var_val
+        elif max_or_min == "min":
+            obj_func_val = -var_val
+        return obj_func_val
+
+    return objectiveFunction
