dbe7e29553bccf6caa9baa9cfcf4bb7b00ba1285
Alechan <mail@mail.com>
2018-09-26 05:49:39 -0300

    vectorial analysis: first steps in script

--- python/vectorial_analysis.py
+++ python/vectorial_analysis.py
@@ -1,64 +1,88 @@
 # Std
-import numpy
-from scipy.optimize import minimize
-import pybobyqa
-
-# This project
-import modelica_interface.build_model as build_model
+import os
+import sys
+import argparse
+import logging  # instead of prints
+import json
 import filesystem.files_aux as files_aux
 
+# Mine
+import running.sweep
+import plotting.plot_sweep as plot_sweep
 
+logger = logging.getLogger("-Vectorial Sens Calculator-")
+script_description = "Find parameters values that maximize or minimize a variable"
+
+
+# Mine
 def main():
-    # Model information
-    model_name = "SystemDynamics.WorldDynamics.World3.Scenario_1"
-    model_file_path = files_aux.moFilePathFromJSONMoPath("resource/sys_dyn/pw_fix/SystemDynamics/package.mo")
-    start_time = 1900
-    stop_time = 2100
-    param_names = ["max_tot_fert_norm", "p_fioa_cons_const_1", "p_ind_cap_out_ratio_1",
-                   "p_serv_cap_out_ratio_1", "life_expect_norm", "des_compl_fam_size_norm",
-                   "industrial_capital_init", "p_land_yield_fact_1", "p_nr_res_use_fact_1",
-                   "reproductive_lifetime", "subsist_food_pc", "p_avg_life_ind_cap_1"]
-    target_var_name = "population"
-    x0 =  [12.0, 0.43, 3.0, 1.0, 28.0, 3.8, 210000000000.0, 1.0, 1.0, 30.0, 230.0, 14.0]
-    dest_folder_path = files_aux.makeOutputPath("vectorial_analysis")
-    fun = createObjectiveFunctionForModel(model_name, start_time, stop_time, model_file_path, param_names,
-                                          target_var_name, dest_folder_path)
-    bnds = [[x*0.95,x*1.05] for x in x0]
-    res = minimize(fun, x0, method='L-BFGS-B', bounds=bnds)
-    print(res)
+    # Logging settings
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    # Get arguments from command line call
+    json_file_path, dest_folder_path_arg = getCommandLineArguments()
+    # Define where to write the results
+    if not dest_folder_path_arg:
+        dest_folder_path = files_aux.makeOutputPath("vectorial_analysis")
+    else:
+        dest_folder_path = dest_folder_path_arg
 
-# CURVI VERSION, NEEDS TO BE ADAPTED TO ABOVE FORMAT WITH BNDS, EPSILON, ETC
-# # Std
-# import numpy
-# # Project
-# import fortran_interface.curvif_simplified as curvif_simplified
-# x0 = numpy.array([1,1])
-# obj_func = lambda x: -(x[0] + x[1])
-# epsilon = 0.001
-# x_opt,f_opt = curvif_simplified.curvif_simplified(x0,obj_func,epsilon)
-# print("x_opt:",x_opt)
-# print("f_opt:",f_opt)
+    # Read JSON again (both reads should be refactored into one)
+    analyzeFromJSON(dest_folder_path, json_file_path)
 
 
-def createObjectiveFunctionForModel(model_name, start_time, stop_time, model_file_path, param_names, target_var_name,
-                                    build_folder_path):
-    # Initialize builder
-    model_builder = build_model.ModelicaModelBuilder(model_name, start_time, stop_time, model_file_path)
-    # Build model
-    compiled_model = model_builder.buildToFolderPath(build_folder_path)
+def analyzeFromJSON(dest_folder_path, json_file_path):
+    with open(json_file_path, 'r') as fp:
+        full_json = json.load(fp)
+    # Prepare init args
+    model_mo_path = files_aux.moFilePathFromJSONMoPath(full_json["model_mo_path"])
+    sweep_kwargs = \
+        {
+            "model_name": full_json["model_name"],
+            "model_file_path": model_mo_path,
+            "start_time": full_json["start_time"],
+            "stop_time": full_json["stop_time"],
+            "perturbation_info_per_param": full_json["parameters_to_sweep"],
+            "build_folder_path": dest_folder_path,
 
-    def objectiveFunction(params_vals):
-        # Set the values for each respective parameter
-        for i in range(len(param_names)):
-            p_name = param_names[i]
-            p_val = params_vals[i]
-            compiled_model.setParameterStartValue(p_name, p_val)
-        # Run the simulation
-        var_val = compiled_model.quickSimulate(target_var_name)
-        return var_val
+        }
+    # Initialize sweeper
+    sweep_runner = running.sweep.ParametersSweeper(**sweep_kwargs)
+    # Run sweep
+    sweep_results = sweep_runner.runSweep(dest_folder_path)
+    # Initialize sweep results plotter
+    sweep_plotter = plot_sweep.SweepPlot(sweep_results)
+    # Make folder for plots
+    plot_folder_path = os.path.join(dest_folder_path, "plots")
+    files_aux.makeFolderWithPath(plot_folder_path)
+    # Plot sweep for each var
+    vars_plots_paths = {}
+    for var_name in full_json["vars_to_analyze"]:
+        plot_path = sweep_plotter.plotInFolder(var_name, plot_folder_path)
+        vars_plots_paths[var_name] = plot_path
+    # Add sweep plots to paths dict
+    paths_dict = \
+        {
+            "sweep_plots": vars_plots_paths,
+        }
+    # Write paths dict as json
+    paths_json_str = json.dumps(paths_dict, sort_keys=True, indent=2)
+    paths_json_file_name = "paths.json"
+    paths_json_file_path = os.path.join(dest_folder_path, paths_json_file_name)
+    files_aux.writeStrToFile(paths_json_str, paths_json_file_path)
+    logger.info("Finished. The file {0} has all the sweep files paths.".format(paths_json_file_path))
 
-    return objectiveFunction
 
+# Auxs
+def getCommandLineArguments():
+    parser = argparse.ArgumentParser(description=script_description)
+    parser.add_argument('test_file_path', metavar='test_file_path',
+                        help='The path to the file with the experiment specifications.')
+    parser.add_argument('--dest_folder_path', metavar='dest_folder_path',
+                        help='Optional: The destination folder where to write the sweep files')
+    args = parser.parse_args()
+    return args.test_file_path, args.dest_folder_path
 
+# FIRST EXECUTABLE CODE:
 if __name__ == "__main__":
     main()
+
