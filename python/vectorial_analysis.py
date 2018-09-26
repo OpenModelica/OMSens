f9b2ca12c7f0e183a2a5dd8c7c58d18fc8884f3b
Alechan <mail@mail.com>
2018-09-26 12:22:19 -0300

    vectorial: first working experiment (output is yet to be defined)

--- python/vectorial_analysis.py
+++ python/vectorial_analysis.py
@@ -7,8 +7,7 @@ import json
 import filesystem.files_aux as files_aux
 
 # Mine
-import running.sweep
-import plotting.plot_sweep as plot_sweep
+import vectorial.model_optimizer as model_optimizer_f
 
 logger = logging.getLogger("-Vectorial Sens Calculator-")
 script_description = "Find parameters values that maximize or minimize a variable"
@@ -35,41 +34,45 @@ def analyzeFromJSON(dest_folder_path, json_file_path):
         full_json = json.load(fp)
     # Prepare init args
     model_mo_path = files_aux.moFilePathFromJSONMoPath(full_json["model_mo_path"])
-    sweep_kwargs = \
+    optim_kwargs = \
         {
-            "model_name": full_json["model_name"],
-            "model_file_path": model_mo_path,
-            "start_time": full_json["start_time"],
-            "stop_time": full_json["stop_time"],
-            "perturbation_info_per_param": full_json["parameters_to_sweep"],
-            "build_folder_path": dest_folder_path,
+            "model_file_path"      : model_mo_path,
+            "target_var_name"      : full_json["target_var_name"],
+            "build_folder_path"    : dest_folder_path,
+            "parameters_to_perturb": full_json["parameters_to_perturb"],
+            "model_name"           : full_json["model_name"],
+            "start_time"           : full_json["start_time"],
+            "stop_time"            : full_json["stop_time"],
+            "max_or_min"           : full_json["max_or_min"],
 
         }
-    # Initialize sweeper
-    sweep_runner = running.sweep.ParametersSweeper(**sweep_kwargs)
-    # Run sweep
-    sweep_results = sweep_runner.runSweep(dest_folder_path)
-    # Initialize sweep results plotter
-    sweep_plotter = plot_sweep.SweepPlot(sweep_results)
-    # Make folder for plots
-    plot_folder_path = os.path.join(dest_folder_path, "plots")
-    files_aux.makeFolderWithPath(plot_folder_path)
-    # Plot sweep for each var
-    vars_plots_paths = {}
-    for var_name in full_json["vars_to_analyze"]:
-        plot_path = sweep_plotter.plotInFolder(var_name, plot_folder_path)
-        vars_plots_paths[var_name] = plot_path
-    # Add sweep plots to paths dict
-    paths_dict = \
-        {
-            "sweep_plots": vars_plots_paths,
-        }
-    # Write paths dict as json
-    paths_json_str = json.dumps(paths_dict, sort_keys=True, indent=2)
-    paths_json_file_name = "paths.json"
-    paths_json_file_path = os.path.join(dest_folder_path, paths_json_file_name)
-    files_aux.writeStrToFile(paths_json_str, paths_json_file_path)
-    logger.info("Finished. The file {0} has all the sweep files paths.".format(paths_json_file_path))
+    # Initialize optimizer
+    model_optimizer = model_optimizer_f.ModelOptimizer(**optim_kwargs)
+    # Run optimization
+    x_opt_dict, f_opt = model_optimizer.optimize(full_json["percentage"], full_json["epsilon"])
+    # BORRAR:
+    # # Initialize sweep results plotter
+    # sweep_plotter = plot_sweep.SweepPlot(sweep_results)
+    # # Make folder for plots
+    # plot_folder_path = os.path.join(dest_folder_path, "plots")
+    # files_aux.makeFolderWithPath(plot_folder_path)
+    # # Plot sweep for each var
+    # vars_plots_paths = {}
+    # for var_name in full_json["vars_to_analyze"]:
+    #     plot_path = sweep_plotter.plotInFolder(var_name, plot_folder_path)
+    #     vars_plots_paths[var_name] = plot_path
+    # # Add sweep plots to paths dict
+    # paths_dict = \
+    #     {
+    #         "sweep_plots": vars_plots_paths,
+    #     }
+    # # Write paths dict as json
+    # paths_json_str = json.dumps(paths_dict, sort_keys=True, indent=2)
+    # paths_json_file_name = "paths.json"
+    # paths_json_file_path = os.path.join(dest_folder_path, paths_json_file_name)
+    # files_aux.writeStrToFile(paths_json_str, paths_json_file_path)
+    # logger.info("Finished. The file {0} has all the sweep files paths.".format(paths_json_file_path))
+    # # BORRAR^
 
 
 # Auxs
