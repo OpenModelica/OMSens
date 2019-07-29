fb94ae68d2d490453df19c4ad0ddf14ec1ab4e7b
pedro <admin@aristas.com.ar>
2019-07-29 13:30:15 -0300

    funcionando full con tiempo final fijo generacion de histogramas de parametro

--- callable_methods/plot_histogram.py
+++ callable_methods/plot_histogram.py
@@ -1,10 +1,12 @@
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
+import os
 
 import matplotlib.pyplot as plt
 import logging
 import argparse
+import pandas as pd
 
 logger = logging.getLogger("--Histogram Plotter--")
 script_description = "Find parameters values that maximize or minimize a variable and plot the results"
@@ -21,20 +23,52 @@ def main():
     parser.add_argument('--filename_path',
                         metavar='filename_path',
                         help='Filename path for output png file')
+    parser.add_argument('--parameter',
+                        metavar='parameter',
+                        help='Parameter for which to make histogram of values at specified time')
+    parser.add_argument('--time_value',
+                        metavar='time_value',
+                        help='Specified time of simulation in which to measure value of specified parameter')
+    parser.add_argument('--runs_path',
+                        metavar='runs_path',
+                        help='Results path directory (from where to get simulation results)')
     args = parser.parse_args()
 
     filename_path = args.filename_path
+    parameter = args.parameter
+    runs_path = args.runs_path + "/" + "perturbed/"
+    time_value = args.time_value
 
-    a = np.array([
-        -0.86906864, -0.72122614, -0.18074998, -0.57190212, -0.25689268, -1., 0.68713553, 0.29597819, 0.45022949,
-        0.37550592, 0.86906864, 0.17437203, 0.48704826, 0.2235648, 0.72122614, 0.14387731, 0.94194514
-    ])
+    vals = []
+    for root, directory, files in os.walk(runs_path):
+        for filename in files:
+            z = pd.read_csv(runs_path + filename, index_col=False).dropna()
+            vals.append(float(z[parameter].tolist()[-1]))
+    df = pd.DataFrame({parameter: vals})
+    vals = df[parameter].values
 
-    fig = plt.hist(a, normed=0)
-    plt.title(filename_path)
-    plt.xlabel("value")
+    # Set number of bins
+    binwidth = 0.1
+    bins = np.linspace(np.round(min(vals), 2), np.round(max(vals), 2), 1 / binwidth)
+    # with open('/home/omsens/Documents/OMSens/callable_methods/zzz.txt', 'w+') as zzz:
+    #     # zzz.write(str(bins))
+    #     zzz.write(str(1/binwidth) + str(vals))
+
+    # Generate histogram
+    fig = plt.hist(vals, bins=bins)
+
+    plt.title("Time: " + time_value + " & " + "Param: " + parameter)
+    plt.xlabel(parameter)
     plt.ylabel("Frequency")
     plt.savefig(filename_path)
+
+
+def set_bins(vals):
+    mn = min(vals)
+    mx = max(vals)
+    if mx < 1:
+        return [i for i in range(0, 1, 0.1)]
+    return [i for i in range(0, round(mx, 1))]
 
 
 if __name__ == "__main__":
